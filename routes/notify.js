616e758de24fd4dc89898e10606696c09eac30f5
lemon <wpliuzd@163.com>
2016-05-30 18:32:52 +0800

    be sure of the notification request method

--- routes/notify.js
+++ routes/notify.js
@@ -2,8 +2,9 @@ var express = require('express');
 var notify = require('../service/ccb_notify.js');
 var router = express.Router();
 
-router.all('/', function(req, res, next) {
+router.get('/', function(req, res, next) {
 	notify.verify(req, res);
 });
+
 
 module.exports = router;
